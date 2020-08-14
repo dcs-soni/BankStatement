8d45624de2d8609f55c48e9224b305aeef82c584
Divyanshu Soni <divyanshusoni52@gmail.com>
2020-08-14 16:59:36 +0530

    accountid

--- bank_statement.py
+++ bank_statement.py
@@ -1,9 +1,10 @@
 #Bank Statement
 class Account:
-    def __init__(self,username,password,amount):
+    def __init__(self,username,password,amount,accountId):
         self.username = username
         self.password = password
         self.amount = amount
+        self.accountId = accountId
     
     # def __repr__(self):
     #     return f"Account({self.username},{self.amount})"
@@ -13,30 +14,31 @@ class Banking:
     def __init__(self):
         self.accounts = []
 
-    def create_account(self,username,password,amount):
+    def create_account(self,username,password,amount,accountId):
         for i in self.accounts:
-            if i.username == username:
+            if i.username == username or i.accountId == accountId:
                 return "Username already exists"
         
 
-        new = Account(username,password,amount)
+        new = Account(username,password,amount,accountId)
         self.accounts.append(new)
         print("\n--------Account created successfully--------\n")
         print("\tAccount Name:     " + new.username )
+        print("\tAccount Id:       " + str(new.accountId))
         return "\tBalance:          " + str(new.amount)
         
                 
 
     
-    def get_account(self, username, password):
+    def get_account(self, username, password,accountId):
         for i in self.accounts:
-            if i.username == username and i.password == password:
+            if i.username == username and i.password == password or i.accountId == accountId:
                 return i
-            
+             
         return False
     
-    def withdraw(self,username,password,amount):
-        account = self.get_account(username,password)
+    def withdraw(self,username,password,amount,accountId):
+        account = self.get_account(username,password,accountId)
         if account == False:
             return "Invalid Username/Password"
         
@@ -50,8 +52,8 @@ class Banking:
             return "Invalid amount"
         
 
-    def deposit(self, username,password,amount):
-        account = self.get_account(username,password)
+    def deposit(self, username,password,amount,accountId):
+        account = self.get_account(username,password,accountId)
         if account == False:
             return "Invalid Username/Password"
         
@@ -61,44 +63,44 @@ class Banking:
         else:
             return "Invalid Amount"
 
-    def balance(self,username,password):
-        account = self.get_account(username,password)
+    def balance(self,username,password,accountId):
+        account = self.get_account(username,password,accountId)
         if account == False:
             return "Invalid Username/Password"
         
         for i in self.accounts:
             if i.username == username and i.password == password:
-                return "Balance in the account " + str(account.username) + " is " + str(account.amount)
+                return "\tBalance:          " + str(account.amount)
 
         
 BANK = Banking()
 
-output = BANK.create_account(username = "Divyanshu", password = "abcytr24", amount = 100000)
-print(output)
+acc1 = BANK.create_account(username = "Divyanshu", password = "abcytr24", amount = 100000, accountId = 1234567)
+print(acc1)
 
-output = BANK.deposit("Divyanshu","abcytr24" , 2000)
-print(output)
+acc1 = BANK.deposit("Divyanshu","abcytr24" , 2000, 1234567)
+print(acc1)
 
-output = BANK.withdraw("Divyanshu","abcytr24",5000)
-print(output)
+acc1 = BANK.withdraw("Divyanshu","abcytr24",5000, 1234567)
+print(acc1)
+
+print("-----------------------------------")
+acc1 = BANK.balance("Divyanshu","abcytr24", 1234567)
+print(acc1)
+# -----------------------------------------------------------
+
+acc2 = BANK.create_account(username = "Divyanshu Soni", password = "abcygege4", amount = 10445500, accountId = 234567)
+print(acc2)
+
+acc2 = BANK.deposit("Divyanshu Soni","abcygege4" , 232000, 234567)
+print(acc2)
+
+acc2 = BANK.withdraw("Divyanshu Soni","abcygege4",5675000, 234567)
+print(acc2)
 print("-----------------------------------")
 
-output = BANK.balance("Divyanshu","abcytr24")
-print(output)
-
-
-output = BANK.create_account(username = "Divyanshu Soni", password = "abcygege4", amount = 10445500)
-print(output)
-
-output = BANK.deposit("Divyanshu Soni","abcygege4" , 232000)
-print(output)
-
-output = BANK.withdraw("Divyanshu Soni","abcygege4",5675000)
-print(output)
-print()
-
-output = BANK.balance("Divyanshu Soni","abcygege4")
-print(output)
+acc2 = BANK.balance("Divyanshu Soni","abcygege4", 234567)
+print(acc2)
 
 
 
